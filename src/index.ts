87c430ceef1b9cac0e896c9d879392ba6dc511c3
Tangerie <josh.cavill01@gmail.com>
2022-11-09 18:27:57 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Client } from "discord.js";
+
+console.log("Starting Client");
+
+const client = new Client({
+    intents: [
+        "Guilds",
+        "GuildMembers",
+        "GuildBans",
+        "GuildEmojisAndStickers",
+        "GuildIntegrations",
+        "GuildWebhooks",
+        "GuildInvites",
+        "GuildVoiceStates",
+        "GuildPresences",
+        "GuildMessages",
+        "GuildMessageReactions",
+        "GuildMessageTyping",
+        "DirectMessages",
+        "DirectMessageReactions",
+        "DirectMessageTyping",
+        "MessageContent",
+        "GuildScheduledEvents",
+        "AutoModerationConfiguration",
+        "AutoModerationExecution"
+    ]
+})
